c8478be327518e9b605646ec7d809f1a5d1ed25a
lfoster1885-prog <lfoster1885@gmail.com>
2026-03-07 11:29:09 -0700

    Modal animation

---
+++ index.js
@@ -0,0 +1,40 @@
+// template_u1g0j7w
+//service_mnecg0v
+//kdBjSdKTL9fvwXdhZ
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+
+    // show the loading overlay
+    loading.classList.add('modal__overlay--visible');
+
+    emailjs
+        .sendForm(
+            "service_mnecg0v",
+            "template_u1g0j7w",
+            event.target,
+            "kdBjSdKTL9fvwXdhZ"
+        )
+        .then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList.add("modal__overlay--visible");
+        })
+        .catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert("The email service is temporarily unavailable. Please contact me directly at luke.foster.web.developer@gmail.com");
+        });
+}
+
+// TOGGLE MODAL
+let isModalOpen = false
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    console.log('toggleModal()')
+    isModalOpen = !isModalOpen
+    document.body.classList.add("modal--open");
+}
